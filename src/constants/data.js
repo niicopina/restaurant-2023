c1e1ed601efa020462d66637af8aa1ec047e427e
nicopina <pinanicolasagustin@gmail.com>
2023-03-09 18:16:54 -0300

    1commit

---
+++ src/constants/data.js
@@ -0,0 +1,109 @@
+const wines = [
+    { 
+      title: 'Chapel Hill Shiraz',
+      price: '$56',
+      tags: 'ARG | bottle',
+    },
+    {
+      title: 'Vina Alberdi Reserva',
+      price: '$32',
+      tags: 'ESP | bottle',
+    },
+    {
+      title: 'La Crema Pinot Noir',
+      price: '$48',
+      tags: 'USA | bottle',
+    },
+    {
+      title: 'Antinori Tignanello',
+      price: '$125',
+      tags: 'ITA | bottle',
+    },
+    {
+      title: 'Chateau Margaux',
+      price: '$595',
+      tags: 'FRA | bottle',
+    },
+    {
+      title: 'Beringer Private Reserve',
+      price: '$125',
+      tags: 'USA | bottle',
+    },
+    {
+      title: 'Marques de Murrieta Gran Reserva',
+      price: '$59',
+      tags: 'ESP | bottle',
+    },
+    {
+      title: 'Catena Zapata Nicolas Malbec',
+      price: '$125',
+      tags: 'ARG | bottle',
+    },
+    {
+      title: 'Opus One',
+      price: '$350',
+      tags: 'USA | bottle',
+    },
+    {
+      title: 'Veuve Clicquot Brut',
+      price: '$75',
+      tags: 'FRA | bottle',
+    }
+  ];
+  
+// module.exports = { wines };
+
+const cocktails = [
+    {
+      name: 'Margarita',
+      price: '$10',
+      tags: 'tequila | lime juice | triple sec | simple syrup',
+    },
+    {
+      name: 'Old Fashioned',
+      price: '$12',
+      tags: 'bourbon | sugar | bitters | orange peel',
+    },
+    {
+      name: 'Negroni',
+      price: '$14',
+      tags: 'gin | sweet vermouth | Campari | orange peel',
+    },
+    {
+      name: 'Moscow Mule',
+      price: '$10',
+      tags: 'vodka | ginger beer | lime juice | mint sprig',
+    },
+    {
+      name: 'Sazerac',
+      price: '$14',
+      tags: 'rye whiskey | absinthe | sugar | Peychauds bitters | lemon peel',
+    },
+    {
+      name: 'Whiskey Sour',
+      price: '$12',
+      tags: 'bourbon | lemon juice | simple syrup | egg white',
+    },
+    {
+      name: 'Manhattan',
+      price: '$14',
+      tags: 'rye whiskey | sweet vermouth | bitters | cherry',
+    },
+    {
+      name: 'Pina Colada',
+      price: '$10',
+      tags: 'rum | pineapple juice | coconut cream | lime juice',
+    },
+    {
+      name: 'French 75',
+      price: '$14',
+      tags: 'gin | lemon juice | simple syrup | Champagne',
+    },
+    {
+      name: 'Tom Collins',
+      price: '$10',
+      tags: 'gin | lemon juice | simple syrup | club soda | orange slice',
+    }
+  ];
+  
+// module.exports = { cocktails };
