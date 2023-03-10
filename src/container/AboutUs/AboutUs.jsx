f07c1367ecb94009817f11a98fe83ff909543f64
nicopina <pinanicolasagustin@gmail.com>
2023-03-09 22:55:00 -0300

    aboutUs

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
-
+import {images} from '../../constants'
 import './AboutUs.css';
 
 const AboutUs = () => (
-  <div>
-    AboutUs
+  <div className='app__aboutus app__bg flex__center section__padding' id='about'>
+    <div className="app__aboutus-overlay flex__center">
+      <img src={images.G} alt='G'/>
+    </div>
+    <div className="app__aboutus-content flex__center">
+
+      <div className="app__aboutus-content_about">
+        <h1 className='headtext__cormorant'>About Us</h1>
+        <img src={images.spoon} alt='spooon' className='spoon-img'/>
+        <p className='p__opensans'>Lorem ipsum dolor sit amet consectetur 
+        adipisicing elit. Quia ex harum consequatur accusanti</p>
+        <button type='button' className='custom__button'>Know More</button>
+      </div>
+
+      <div className="app__aboutus-content_knife flex__center">
+        <img src={images.knife} alt='knife'/>
+      </div>
+
+      <div className="app__aboutus-content_history">
+        <h1 className='headtext__cormorant'>Our History</h1>
+        <img src={images.spoon} alt='spooon' className='spoon-img'/>
+        <p className='p__opensans'>Lorem ipsum dolor sit amet consectetur 
+        adipisicing elit. Quia ex harum consequatur accusanti</p>
+        <button type='button' className='custom__button'>Know More</button>
+      </div>
+    </div>
   </div>
 );
 
