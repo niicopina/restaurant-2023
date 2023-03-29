38f1026da9e48d97f6a45901b4baa8a9a8690462
nicopina <pinanicolasagustin@gmail.com>
2023-03-29 03:22:25 -0300

    footer

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
-
+import SubHeading from '../SubHeading/SubHeading'
 import './Newsletter.css';
 
 const Newsletter = () => (
-  <div>
-    Newsletter
+  <div className="app__newsletter">
+    <div className="app__newsletter-heading">
+      <SubHeading title='Newsletter'/>
+      <h1 className='headtext__cormorant'>Subscribe to our Newsletter</h1>
+      <p className='p__opensans'>And never miss latest updates</p>
+    </div>
+    <div className="app__newsletter-input flex__center">
+      <input type="email" placeholder='enter your e-mail adress'/>
+      <button className='custom__button'>Subscribe</button>
+    </div>
   </div>
 );
 
--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,10 +1,44 @@
 import React from 'react';
-
+import { FiFacebook, FiTwitter, FiInstagram} from 'react-icons/fi'
+import { FooterOverlay, Newsletter} from '../../components'
+import {images} from '../../constants'
 import './Footer.css';
 
 const Footer = () => (
-  <div>
-    Footer
+  <div className="app__footer section__padding">
+    <FooterOverlay/>
+    <Newsletter/>
+
+    <div className="app__footer-links">
+      <div className="app__footer-links_contact">
+        <h1 className='app__footer-headtext'>Contact Us</h1>
+        <p className='p__opensans'>Av. Siempreviva 123, EU</p>
+        <p className='p__opensans'>+54 011 1234 5678</p>
+        <p className='p__opensans'>+54 011 8765 4321</p>
+      </div>
+      <div className="app__footer-links_logo">
+        <img src={images.gericht} alt="footerlogo" />
+        <p className='p__opensans'>"The best way to find yourself 
+                is to lose yourself in the service of others</p>
+                <img src={images.spoon} alt="spoon" className='spoon__img' style={{marginTop: '15px'}}/>
+                <div className="app__footer-links_icons">
+                  <FiFacebook/>
+                  <FiTwitter />
+                  <FiInstagram />
+                </div>
+      </div>
+      <div className="app__footer-links_work">
+        <h1 className='app__footer-headtext'>Working Hours</h1>
+          <p className='p__opensans'>Monday - Friday:</p>
+          <p className='p__opensans'>10:00 am - 23:00 pm</p>
+          <p className='p__opensans'>Saturday - Sunday:</p>
+          <p className='p__opensans'>10:00 am - 02:00 am</p>
+        </div>
+    </div>
+
+    <div className="footer__copyright">
+      <p className='p__opensans'>2023 Gerích - All Rights Reserved</p>
+    </div>
   </div>
 );
 
